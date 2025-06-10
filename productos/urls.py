37ad9dad69f841a34c525b13996523fe81958428
BLipax <p.trejo.g@gmail.com>
2025-06-10 03:07:32 -0400

    Agregar botón de eliminar producto con autenticación

--- productos/urls.py
+++ productos/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductoViewSet, OrdenViewSet, ProductoList
-from .views import iniciar_pago, actualizar_stock, pago_exitoso, webpay_respuesta
+from .views import ProductoViewSet, OrdenViewSet, RegistroView, iniciar_pago, actualizar_stock, pago_exito, webpay_respuesta
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,10 +9,14 @@ router.register('productos', ProductoViewSet)
 router.register('ordenes', OrdenViewSet)
 
 urlpatterns = [
-    path('lista/', ProductoList.as_view(), name='producto-list'),
-    path('iniciar_pago/', iniciar_pago, name='iniciar_pago'),
-    path('webpay_respuesta/', webpay_respuesta, name='webpay_respuesta'),
-    path("pago_exitoso/", pago_exitoso, name="pago_exitoso"),
-    path('actualizar_stock/', actualizar_stock, name='actualizar_stock'),
-    path('', include(router.urls)),  # ahora esto manejará /api/productos/
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('registro/', RegistroView.as_view(), name='registro'),
+    path('iniciar-pago/', iniciar_pago, name='iniciar_pago'),
+    path('webpay-respuesta/', webpay_respuesta, name='webpay_respuesta'),
+    path('pago-exito/', pago_exito, name='pago_exito'),
+    path('actualizar-stock/', actualizar_stock, name='actualizar_stock'),
+    path('', include(router.urls)),  # Handles /api/productos/, /api/ordenes/
+]
+
+# Serve media files for testing
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
